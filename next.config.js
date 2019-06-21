c93c39122dbf2b492eee1c36ae517fded91f0c52
Vien Dinh <maxvien@gmail.com>
2019-06-22 00:29:37 +0700

    Config to deploy to Heroku

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
-const withTypescript = require("@zeit/next-typescript")
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const dev = process.env.NODE_ENV !== 'production';
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+let config = {};
 
-    return config
-  }
-});
+if (dev) {
+  const withTypescript = require("@zeit/next-typescript")
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+  config = withTypescript({
+    webpack(config, options) {
+      // Do not run type checking twice:
+      if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+
+      return config
+    }
+  });
+}
+
+module.exports = config;
