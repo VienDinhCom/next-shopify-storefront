f74d04fd1be4ec1f6ba4163a74bce0aa30461b9f
Vien Dinh <maxvien@gmail.com>
2019-06-21 23:13:15 +0700

    Deploy with Now

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const withTypescript = require("@zeit/next-typescript")
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = withTypescript({
+  target: 'serverless',
   webpack(config, options) {
     // Do not run type checking twice:
     if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
     return config
   }
-})
+});
