6a8f8291cdf70764f5236d703223e0bf24990a38
Vien Dinh <maxvien@gmail.com>
2019-06-21 23:40:03 +0700

    Remove target: 'serverless',

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withTypescript = require("@zeit/next-typescript")
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = withTypescript({
-  target: 'serverless',
   webpack(config, options) {
     // Do not run type checking twice:
     if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
