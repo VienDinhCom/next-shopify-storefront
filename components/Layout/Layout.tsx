1ae5116d16435c9b84b9c764893a5bccc7e3d3cf
Vien Dinh <maxvien@gmail.com>
2019-06-23 17:27:51 +0700

    Update Head

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import AppBar from './AppBar';
@@ -9,6 +10,14 @@ interface Props {
 function Layout({ children }: Props) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+        <meta name="theme-color" content="#556cd6" />
+        <link rel="icon" href="https://reactjs.org/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <title>Next Shopify Storefront</title>
+      </Head>
       <AppBar />
       <br />
       <br />
