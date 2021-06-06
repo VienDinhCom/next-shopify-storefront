443600b2464239de307af52bf113cb262cbee90b
Vien Dinh <maxvien@gmail.com>
2021-06-06 11:26:50 +0700

    fix env

--- src/services/env.service.ts
+++ src/services/env.service.ts
@@ -1,10 +1,4 @@
-type Variable = 'NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_ENDPOINT' | 'NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN';
-
 export namespace EnvService {
-  export function get(variable: Variable) {
-    return process.env[variable]!;
-  }
-
   export function isNode() {
     return !process.browser;
   }
--- src/services/shopify.service.ts
+++ src/services/shopify.service.ts
@@ -1,9 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
-import { EnvService } from './env.service';
 import { getSdk } from './shopify/generated';
 
-const endpoint = EnvService.get('NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_ENDPOINT');
-const accessToken = EnvService.get('NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN');
+const endpoint = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_ENDPOINT;
+const accessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+
+console.log({ endpoint, accessToken });
 
 export const client = new GraphQLClient(endpoint, {
   headers: { 'X-Shopify-Storefront-Access-Token': accessToken },
