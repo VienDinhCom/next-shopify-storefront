681a376a6d08f3b5387367fcce666b8a85dc76e2
Vien Dinh <maxvien@gmail.com>
2021-06-06 11:27:43 +0700

    micro changes

--- src/services/shopify.service.ts
+++ src/services/shopify.service.ts
@@ -4,8 +4,6 @@ import { getSdk } from './shopify/generated';
 const endpoint = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_ENDPOINT;
 const accessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
 
-console.log({ endpoint, accessToken });
-
 export const client = new GraphQLClient(endpoint, {
   headers: { 'X-Shopify-Storefront-Access-Token': accessToken },
 });
