4bcad80134f8043073ac418ebe2a6e489de510ba
Vien Dinh <maxvien@gmail.com>
2021-08-24 11:28:44 +0700

    add product item

---
+++ src/components/snippets/product-item/product-item.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { CardActionArea } from '@material-ui/core';
+
+export interface ProductItemProps {
+  handle: string;
+  title: string;
+  description: string;
+  image: {
+    src: string;
+    alt?: string;
+  };
+}
+
+export const ProductItem: React.FC<ProductItemProps> = (props) => {
+  const router = useRouter();
+  const url = `/products/${props.handle}`;
+
+  return (
+    <Card>
+      <CardActionArea
+        href={url}
+        onClick={(event) => {
+          event.preventDefault();
+          router.push(url);
+        }}
+      >
+        <CardMedia sx={{ height: 350 }} image={props.image.src} title={props.image.alt} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
