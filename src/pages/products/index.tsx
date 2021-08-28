c7b894324920acefcfdab46448a3a05bc3762c7a
Vien Dinh <maxvien@gmail.com>
2021-08-28 11:14:46 +0700

    Refactor ProductService

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,14 +1,18 @@
-import { InfiniteData } from 'react-query';
+import last from 'lodash/last';
+import { PromiseValue } from 'type-fest';
+import { InfiniteData, useInfiniteQuery } from 'react-query';
+import { ProductList } from '@app/components/sections/product-list';
 import { DefaultLayout } from '@app/components/layouts/default-layout/default-layout';
-import { ProductService, GetProductListQuery } from '@app/services/product.service';
-import { ProductList, ProductListProps } from '@app/components/sections/product-list';
+
+import { PRODUCT_LIST_QUERY } from '@app/constants/query.constant';
+import { ProductService } from '@app/services/product.service';
 
 interface Props {
-  initialData: InfiniteData<GetProductListQuery>;
+  initialData: InfiniteData<PromiseValue<ReturnType<typeof ProductService.getList>>>;
 }
 
 Page.getInitialProps = async (): Promise<Props> => {
-  const firstPage = await ProductService.getList();
+  const firstPage = await ProductService.getList({});
 
   return {
     initialData: { pages: [firstPage], pageParams: [null] },
@@ -16,30 +20,22 @@ Page.getInitialProps = async (): Promise<Props> => {
 };
 
 export default function Page({ initialData }: Props) {
-  const productList = ProductService.useList({ options: { initialData } });
-
-  const products: ProductListProps['products'] = productList.data!.pages.flatMap((page) => {
-    return page.products.edges.map((product) => {
-      return {
-        id: product.node.id,
-        handle: product.node.handle,
-        title: product.node.title,
-        description: product.node.description,
-        image: {
-          src: product.node.images.edges[0].node.transformedSrc as string,
-          alt: product.node.images.edges[0].node.altText || '',
-        },
-        price: {
-          amount: product.node.priceRange.minVariantPrice.amount,
-          currencyCode: product.node.priceRange.minVariantPrice.currencyCode,
-        },
-      };
-    });
-  });
+  const productList = useInfiniteQuery(
+    PRODUCT_LIST_QUERY,
+    ({ pageParam }) => ProductService.getList({ after: pageParam }),
+    {
+      initialData,
+      getNextPageParam: (lastPage) => {
+        if (lastPage.pageInfo.hasNextPage) {
+          return last(lastPage.products)?.cursor;
+        }
+      },
+    }
+  );
 
   return (
     <DefaultLayout>
-      <ProductList products={products} pagination={productList} />
+      <ProductList products={productList.data?.pages.flatMap(({ products }) => products)!} pagination={productList} />
     </DefaultLayout>
   );
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,43 +1,41 @@
-import last from 'lodash/last';
-import { useInfiniteQuery, UseInfiniteQueryOptions } from 'react-query';
-import { PRODUCT_LIST_QUERY } from '@app/constants/query.constant';
-import {
-  ShopifyService,
-  GetProductListQueryVariables,
-  GetProductListQuery,
-  GetProductSingleQueryVariables,
-} from './shopify.service';
+import { ProductListProps } from '@app/components/sections/product-list';
+import { ShopifyService, GetProductListQueryVariables, GetProductSingleQueryVariables } from './shopify.service';
 
 export namespace ProductService {
-  interface GetListInput {
-    variables?: GetProductListQueryVariables;
-  }
-
-  export function getList(input?: GetListInput) {
-    return ShopifyService.getProductList(input?.variables);
-  }
-
-  interface UseListInput extends GetListInput {
-    options?: UseInfiniteQueryOptions<GetProductListQuery, Error>;
-  }
-
-  export function useList(input?: UseListInput) {
-    return useInfiniteQuery(
-      [PRODUCT_LIST_QUERY, input?.variables],
-      ({ pageParam }) => getList({ variables: { after: pageParam } }),
-      {
-        ...input?.options,
-        getNextPageParam: (lastPage) => {
-          if (lastPage.products.pageInfo.hasNextPage) {
-            return last(lastPage.products.edges)?.cursor;
-          }
-        },
-      }
-    );
-  }
-
   export function getSingle(handle: GetProductSingleQueryVariables['handle']) {
     return ShopifyService.getProductSingle({ handle });
+  }
+
+  export async function getList(variables?: GetProductListQueryVariables) {
+    const {
+      products: { edges, pageInfo },
+    } = await ShopifyService.getProductList(variables);
+
+    type Product = ProductListProps['products'][0];
+
+    interface ProductItem extends Product {
+      cursor: string;
+    }
+
+    const products: ProductItem[] = edges.map(({ node, cursor }) => {
+      return {
+        id: node.id,
+        cursor: cursor,
+        handle: node.handle,
+        title: node.title,
+        description: node.description,
+        image: {
+          src: node.images.edges[0].node.transformedSrc,
+          alt: node.images.edges[0].node.altText || '',
+        },
+        price: {
+          amount: node.priceRange.minVariantPrice.amount,
+          currencyCode: node.priceRange.minVariantPrice.currencyCode,
+        },
+      };
+    });
+
+    return { products, pageInfo };
   }
 }
 
