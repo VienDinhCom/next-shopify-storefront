5130d23303a8b8cbcf5f3238c592066f3662bb0d
Vien Dinh <maxvien@gmail.com>
2021-08-28 10:46:39 +0700

    remove ShopService

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { ShopService, GetShopQuery } from '@app/services/shop.service';
 import { DefaultLayout } from '@app/components/layouts/default-layout';
 
-interface Props {
-  data: GetShopQuery;
-}
-
-Page.getInitialProps = async (): Promise<Props> => {
-  return { data: await ShopService.getIt() };
-};
-
-export default function Page({ data }: Props) {
+export default function Page() {
   return <DefaultLayout>Home</DefaultLayout>;
 }
