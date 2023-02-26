d564cd62b853a33e6771403fbb0abbca4c212f36
Vien Dinh <maxvien@gmail.com>
2023-02-26 16:32:37 +0700

    update deps

--- src/utilities/deps.ts
+++ src/utilities/deps.ts
@@ -1,4 +1,10 @@
-export type { AppProps } from 'next/app';
+export type { AppProps as NextAppProps } from 'next/app';
+export { default as NextImage } from 'next/image';
+
 export type { ReactNode, ReactElement } from 'react';
+export { Fragment } from 'react';
+export { useImmer as useState } from 'use-immer';
 
 export { default as clsx } from 'clsx';
+
+export * from '@headlessui/react';
