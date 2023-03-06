be9540c51d2d1f66466a3cd37780903b26fce30b
Vien Dinh <maxvien@gmail.com>
2023-03-06 16:44:45 +0700

    update scalars for mutations

--- src/utilities/storefront/index.ts
+++ src/utilities/storefront/index.ts
@@ -1,4 +1,4 @@
-import { Thunder } from './zeus';
+import { Thunder, ZeusScalars } from './zeus';
 
 const apiEndpoint = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_ENDPOINT as string;
 const accessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN as string;
@@ -33,20 +33,24 @@ const thunder = Thunder(async (query: string, variables: Record<string, unknown>
   return data;
 });
 
+const scalars = ZeusScalars({
+  URL: {
+    encode: (e) => e as string,
+    decode: (e) => e as string,
+  },
+  Decimal: {
+    encode: (e) => `${e}`,
+    decode: (e) => parseFloat(e as string),
+  },
+});
+
 export const storefront = {
   query: thunder('query', {
-    scalars: {
-      URL: {
-        encode: (e) => e as string,
-        decode: (e) => e as string,
-      },
-      Decimal: {
-        encode: (e) => `${e}`,
-        decode: (e) => parseFloat(e as string),
-      },
-    },
+    scalars,
   }),
-  mutation: thunder('mutation'),
+  mutation: thunder('mutation', {
+    scalars,
+  }),
 };
 
 export * from './zeus';
