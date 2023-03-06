9ffc0d212d2193b4c1760dc9a978a27b01ccfa7f
Vien Dinh <maxvien@gmail.com>
2023-03-06 13:53:54 +0700

    fix layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -9,9 +9,7 @@ export function DefaultLayout(props: Props) {
   return (
     <>
       <Header />
-      <main className='mx-auto max-w-7xl p-6 lg:px-8'>
-        {props.children}
-      </main>
+      <main className="mx-auto max-w-7xl p-6 lg:px-8">{props.children}</main>
     </>
   );
 }
