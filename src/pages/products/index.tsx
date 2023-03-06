d1269223b0ccb6c19b791d7e93d2daba4d507b4f
Vien Dinh <maxvien@gmail.com>
2023-03-06 14:22:52 +0700

    create product list ui

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,9 +1,36 @@
 import { DefaultLayout } from '@app/layouts/DefaultLayout';
 
+const products = [
+  {
+    id: 1,
+    name: 'Earthen Bottle',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+];
+
 export default function Page() {
   return (
     <DefaultLayout>
-      <h1 className="bg-black text-white">Product List</h1>
+      <h2 className="sr-only">Products</h2>
+
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {products.map((product) => (
+          <a key={product.id} href={product.href} className="group">
+            <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+              <img
+                src={product.imageSrc}
+                alt={product.imageAlt}
+                className="h-full w-full object-cover object-center group-hover:opacity-75"
+              />
+            </div>
+            <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+            <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+          </a>
+        ))}
+      </div>
     </DefaultLayout>
   );
 }
