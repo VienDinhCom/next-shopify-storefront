d339aa0329197a2e495c0bfc84941357244a9330
Vien Dinh <maxvien@gmail.com>
2023-03-07 10:29:46 +0700

    update Button

--- src/snippets/Button.tsx
+++ src/snippets/Button.tsx
@@ -1,43 +1,50 @@
-import { ReactNode, clsx } from '@app/utilities/deps';
+import { ReactNode, clsx, NextLink } from '@app/utilities/deps';
 
 interface Props {
   onClick?: () => void;
   children: ReactNode;
-  color: 'primary';
+  color: 'primary' | 'danger';
   size: 'xs' | 'sm' | 'md';
+  href?: string;
+  disabled?: boolean;
 }
 
 export function Button(props: Props) {
   const colors = {
-    primary: {
-      default: {
-        color: 'text-white',
-        background: 'bg-primary-600',
-      },
-      hover: {
-        color: 'text-white',
-        background: 'hover:bg-primary-500',
-      },
-    },
+    primary: clsx('bg-primary-600 text-white hover:bg-primary-500 disabled:bg-primary-400'),
+    danger: clsx('bg-danger-600 text-white hover:bg-danger-500 disabled:bg-danger-400'),
   };
 
   const sizes = {
-    xs: 'py-1 px-2 text-xs',
-    sm: 'py-2 px-3 text-sm',
-    md: 'py-3 px-4 text-base',
+    xs: clsx('py-1 px-2 text-xs'),
+    sm: clsx('py-2 px-3 text-sm'),
+    md: clsx('py-3 px-4 text-base'),
   };
+
+  if (props.href) {
+    return (
+      <NextLink
+        href={props.href}
+        className={clsx(
+          'pointer-events-auto rounded-md font-semibold leading-5',
+          colors[props.color],
+          sizes[props.size]
+        )}
+      >
+        {props.children}
+      </NextLink>
+    );
+  }
 
   return (
     <button
       onClick={props.onClick}
       className={clsx(
-        'pointer-events-auto rounded-md font-semibold leading-5',
-        colors[props.color].default.color,
-        colors[props.color].default.background,
-        colors[props.color].hover.color,
-        colors[props.color].hover.background,
+        'pointer-events-auto rounded-md font-semibold leading-5 ',
+        colors[props.color],
         sizes[props.size]
       )}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
