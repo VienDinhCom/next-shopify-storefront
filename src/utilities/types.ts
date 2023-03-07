3c5cc34b1442248e25d3029a1a8c7c2a2074d94c
Vien Dinh <maxvien@gmail.com>
2023-03-07 10:29:36 +0700

    add color and size types

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -1,1 +1,4 @@
 export type { AsyncReturnType } from 'type-fest';
+
+export type Color = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark';
+export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
