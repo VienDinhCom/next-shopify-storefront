60b2a7b3a828e4590f9f92c0c486bbbbe0f31533
Vien Dinh <maxvien@gmail.com>
2023-04-05 17:15:16 +0700

    feat: add react intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@app/assets/style.css';
+import { IntlProvider } from 'react-intl';
 import ProgressBar from 'nextjs-progressbar';
 import { NextAppProps } from '@app/utilities/deps';
 
 export default function App({ Component, pageProps }: NextAppProps) {
   return (
-    <>
+    <IntlProvider locale="en">
       <ProgressBar color="orange" />
       <Component {...pageProps} />
-    </>
+    </IntlProvider>
   );
 }
