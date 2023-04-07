6494bdfe8075627628c4ba9ba5ca50bf47bbb1cd
Vien Dinh <maxvien@gmail.com>
2023-04-07 11:34:31 +0700

    feat: get selected variant from selected options

--- src/snippets/VariantSelector.tsx
+++ src/snippets/VariantSelector.tsx
@@ -1,5 +1,5 @@
-import { DataProps } from '@app/utilities/deps';
-import { uniqBy } from 'lodash';
+import { DataProps, useState } from '@app/utilities/deps';
+import { uniqBy, isEqual } from 'lodash';
 import type { fetchProductSingleSection } from '@app/sections/ProuctSingleSection';
 
 interface Props {
@@ -7,19 +7,32 @@ interface Props {
 }
 
 export function VariantSelector(props: Props) {
-  const options: Record<string, { value: string; avalable: boolean }[]> = {};
+  const [selectedOptions, setSelectedOptions] = useState({});
+
+  const options: Record<string, { value: string; avalable: boolean; selected: boolean }[]> = {};
 
   props.variants.nodes.forEach((node) => {
     node.selectedOptions.forEach(({ name, value }) => {
       if (options[name]) {
-        options[name] = uniqBy([...options[name], { value, avalable: true }], 'value');
+        options[name] = uniqBy([...options[name], { value, avalable: true, selected: false }], 'value');
       } else {
         options[name] = [];
       }
     });
   });
 
+  const selectedVariant = props.variants.nodes.find((variant) => {
+    const options = variant.selectedOptions.reduce(
+      (previous, current) => ({ ...previous, [current.name]: current.value }),
+      {}
+    );
+
+    return isEqual(selectedOptions, options);
+  });
+
   console.log('options', options);
+  console.log('selectedOptions', selectedOptions);
+  console.log('selectedVariant', selectedVariant);
 
   return (
     <div>
@@ -27,7 +40,13 @@ export function VariantSelector(props: Props) {
         <div key={key}>
           <div>{key}</div>
           {options[key].map(({ value, avalable }) => (
-            <button className="m-3 border" key={value}>
+            <button
+              className="m-3 border"
+              key={value}
+              onClick={() => {
+                setSelectedOptions({ ...selectedOptions, [key]: value });
+              }}
+            >
               {value}
             </button>
           ))}
