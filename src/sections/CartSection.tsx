55679f51ae120ea43b8c2b1eec5db44e0daff234
Vien Dinh <maxvien@gmail.com>
2023-04-11 16:33:47 +0700

    feat: cart section: add selected options for cart lines

--- src/sections/CartSection.tsx
+++ src/sections/CartSection.tsx
@@ -12,6 +12,8 @@ import {
 export function CartSection() {
   const cart = useCart();
 
+  console.log(cart);
+
   return (
     <section>
       <ul role="list" className="-my-6 mb-2 divide-y divide-gray-200">
@@ -38,11 +40,20 @@ export function CartSection() {
                     </h3>
                     <Money className="ml-4" data={line?.cost?.totalAmount!}></Money>
                   </div>
-                </div>
-                <div className="flex flex-1 items-end justify-between text-sm">
-                  <p className="text-gray-500">
-                    Qty <CartLineQuantity></CartLineQuantity>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {line?.merchandise?.selectedOptions?.map((option, index) => (
+                      <span key={option?.name}>
+                        {index ? ' / ' : ''}
+                        {option?.value}
+                      </span>
+                    ))}
                   </p>
+                </div>
+
+                <div className="flex flex-1 items-end justify-between text-sm">
+                  <div className="text-gray-500">
+                    Qty <CartLineQuantity></CartLineQuantity>
+                  </div>
 
                   <div className="flex">
                     <CartLineQuantityAdjustButton
@@ -65,9 +76,9 @@ export function CartSection() {
       <div className="border-t border-gray-200 pt-4">
         <div className="flex justify-between text-base font-medium text-gray-900">
           <p>Subtotal</p>
-          <p>
+          <div>
             <CartCost amountType="subtotal" />
-          </p>
+          </div>
         </div>
         <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
         <div className="mt-6 flex">
