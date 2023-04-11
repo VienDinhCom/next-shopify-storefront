0a6a44475afe1a4786b16fba46c205dcdc0b4340
Vien Dinh <maxvien@gmail.com>
2023-04-11 11:12:05 +0700

    feat: button snippet: improve color

--- src/snippets/Button.tsx
+++ src/snippets/Button.tsx
@@ -3,7 +3,8 @@ import { ReactNode, clsx, NextLink } from '@app/utilities/deps';
 interface Props {
   onClick?: () => void;
   children: ReactNode;
-  color: 'primary' | 'danger';
+  className?: string;
+  color: 'primary' | 'danger' | 'light' | 'dark';
   size: 'xs' | 'sm' | 'md';
   href?: string;
   disabled?: boolean;
@@ -12,6 +13,8 @@ interface Props {
 const colors = {
   primary: clsx('bg-primary-600 text-white hover:bg-primary-500 disabled:bg-primary-400'),
   danger: clsx('bg-danger-600 text-white hover:bg-danger-500 disabled:bg-danger-400'),
+  dark: clsx('bg-gray-900 text-white hover:bg-gray-800 disabled:bg-gray-700'),
+  light: clsx('bg-gray-100 text-white hover:bg-gray-200 disabled:bg-gray-300'),
 };
 
 const sizes = {
@@ -28,7 +31,8 @@ export function Button(props: Props) {
         className={clsx(
           'pointer-events-auto rounded-md font-semibold leading-5',
           colors[props.color],
-          sizes[props.size]
+          sizes[props.size],
+          props.className
         )}
       >
         {props.children}
@@ -42,7 +46,8 @@ export function Button(props: Props) {
       className={clsx(
         'pointer-events-auto rounded-md font-semibold leading-5 ',
         colors[props.color],
-        sizes[props.size]
+        sizes[props.size],
+        props.className
       )}
       disabled={props.disabled}
     >
