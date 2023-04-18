dc0156544730f5d911ff84695498ec080802bf23
Vien Dinh <maxvien@gmail.com>
2023-04-18 15:50:47 +0700

    feat: support seo for cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,9 +1,11 @@
+import { NextSeo } from '@site/utilities/deps';
 import { StoreLayout } from '@site/layouts/StoreLayout';
 import { CartSection } from '@site/sections/CartSection';
 
 export default function Page() {
   return (
     <StoreLayout>
+      <NextSeo title="Cart" />
       <CartSection />
     </StoreLayout>
   );
